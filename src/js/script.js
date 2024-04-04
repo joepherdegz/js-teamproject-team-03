bfabd43b37b684d41e7f03f962191a678a9144d5
d3monw1ngs <tezzielumsit87@gmail.com>
2024-04-04 15:13:48 +0800

    update js files

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 import { BASE_URL } from "./themoviedb-api.js";
-const { language, key } = params.option;
+const { language, key } = options.params;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWU4MjRhM2Q5MjJmNjhiYTIxMWZjZjZkYmRjYjZmNSIsInN1YiI6IjY2MGUwMGE3YzhhNWFjMDE2Mzc5ODIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dVhYcbmcQhxg-GRwDOu4UQPpqCnjC9js7XxT0ubEx7s'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/authentication', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 
 async function fetchTrending() {
   const res =  await fetch(`${BASE_URL}/trending/movie/day?api_key=${key}&language=${language}`);

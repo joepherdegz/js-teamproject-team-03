88f653fa051ab06bfbb758ac5e302fb7db11fdaf
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-04-12 20:25:59 -0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,7 @@
   const API_URL = BASE_URL + '/discover/movie?sort_by-popularity.desc&' + API_KEY;
   const IMG_URL = 'https://image.tmdb.org/t/p/w500';
   const searchURL = BASE_URL + '/search/movie?' + API_KEY;
-  const getGenres = BASE_URL + '/genre/movie/list?' + API_KEY;
-
-
-  const options = {
-    params: {
-      key: API_KEY,
-      query: '',
-      include_adult: false,
-      language: 'en-US',
-      primary_release_year: '',
-      page: 1,
-      region: '',
-      year: '',
-    },
-  };
-
-    
-
+  const getGenres = BASE_URL + '/genre/movie/list?' + API_KEY;   
 
   //localStorage
   let currentMovieTitle, currentMovieID, queue, watched;
